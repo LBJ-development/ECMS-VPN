3f3c73855e563da67921c2e2b86e59302d575944
LBJ-development <ljablonski@ljablonski1-d.NCMECAD.NET>
2015-05-13 17:20:19 -0400

    Case Management
    Adjusting the summary width

--- webapp/components/caseManagement/caseManagementCtrl.js
+++ webapp/components/caseManagement/caseManagementCtrl.js
@@ -27,10 +27,8 @@ angular.module('ECMSapp.caseManagement', [])
 }])
 
 .controller('CaseHeaderCtrl', ['$scope', '$http', function($scope,  $http){
-	console.log("FROM CASE HEADER");
-	console.log($scope);
-
-
+	// console.log("FROM CASE HEADER");
+	// console.log($scope);
 }])
 
 .directive ('caseHeader', ['DataFtry', '$http', function (DataFtry, $http) {
@@ -41,22 +39,78 @@ angular.module('ECMSapp.caseManagement', [])
 	templateUrl: 'components/caseManagement/caseHeader.html',
 	link: function (scope, element, attrs){
 
+		console.log(scope);
+
 		$("#moreBtn").css("display", "none");
 
 		scope.toggleHeader = function(ev) {
 
 			if($("#caseHeaderInfo").is(":visible")) {
 
-				$("#caseHeaderInfo").css("display", "none");
+				$("#caseHeaderInfo").animate(
+					{height: "0", opacity: "0"}, 300, function(){
+						$("#caseHeaderInfo").css("display", "none");
+					});
 				$("#moreBtn").css("display","inline-block");
 				$("#lessBtn").css("display", "none");
 
 			} else {
-				$("#caseHeaderInfo").css("display", "inline-block");
+
+				$("#caseHeaderInfo").css({display: "inline-block"});
+
+				$("#caseHeaderInfo").animate(
+					{height: "100%", opacity: "1"}, 300);
 				$("#moreBtn").css("display", "none");
 				$("#lessBtn").css("display","inline-block");
 				}
 			}
 		}
 	};
-}]);
+}])
+
+.controller('CaseMenuCtrl', ['$scope', '$http', function($scope,  $http){
+	$scope.caseMenuOptions = {
+		contentUrls: [ null, null, "" ]
+		};
+}])
+
+.directive ('caseMenu', ['DataFtry', '$http', function (DataFtry, $http) {
+	return {
+	restrict: 'E',
+	scope :{},
+	controller: 'CaseMenuCtrl',
+	templateUrl: 'components/caseManagement/caseMenu.html',
+	link: function (scope, element, attrs){
+
+		}
+	};
+}])
+
+.controller('CaseSummaryCtrl', ['$scope', '$http', function($scope,  $http){
+	// console.log("FROM CASE HEADER");
+	// console.log($scope);
+}])
+
+.directive ('caseSummary',function ($interval, $window) {
+	return {
+	restrict: 'E',
+	scope :{},
+	controller: 'CaseSummaryCtrl',
+	templateUrl: 'components/caseManagement/caseSummary.html',
+	link: function (scope, element, attrs){
+
+		$window.addEventListener('resize', function() {
+
+			if( window.innerWidth < 1650) {
+
+				$("#info-holder").css('width', (window.innerHeight - 400));
+			}
+
+
+			
+			console.log( window.innerWidth)
+			}, false);
+
+		}
+	};
+});
